babbf81b5e8408017a7e300a8e3466231b4ae7e3
Misako Tatesawa <misa@MisakonoMacBook-Pro.local>
2017-09-25 09:24:35 +0900

    commit_to_heroku

--- db/migrate/20170920153237_create_likes.rb
+++ db/migrate/20170920153237_create_likes.rb
@@ -1,8 +1,6 @@
 class CreateLikes < ActiveRecord::Migration
   def change
     create_table :likes do |t|
-      t.references :recipe, foreign_key: true
-      t.references :user, foreign_key: true
 
 
       t.timestamps null: false
--- db/save/20170919083804_add_user_id_recipes.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdRecipes < ActiveRecord::Migration
-  def change
-    add_column :recipes, :user_id, :integer
-  end
-end
